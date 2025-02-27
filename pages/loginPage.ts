7f526029c6e8638e3ebdd2e957dd581b544144fb
Eleusis Carretero <eleusis.carrtero@gmail.com>
2025-02-26 21:46:26 -0600

    defining method to login

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,3 +1,4 @@
+import { UserCredentials } from "../types/userCredentials";
 import { BasePage } from "./basePage";
 import { Locator, Page } from "@playwright/test";
 
@@ -11,7 +12,16 @@ class LoginPage extends BasePage {
         // Define element locators from login Page
         this.userNameInput = this.page.getByRole('textbox', {name: "username"});  // an input is 'textbox'
         this.passwordInput = this.page.getByRole('textbox', {name: "password"});
-        this.loginButton = this.page.getByRole('textbox', {name: "username"});  // an input is 'textbox'
+        this.loginButton = this.page.getByRole('button', {name: "username"});  // an input is 'textbox'
+    }
+
+    async tryLogin(userCredentials:UserCredentials){
+        // fill user nada credentials
+        this.fillTextBoxElement(this.userNameInput, userCredentials.username);
+        this.fillTextBoxElement(this.passwordInput, userCredentials.password);
+        // click on loginButton
+        this.clickElement(this.loginButton);
+
 
     }
 
